cf3ce4757d00d7da39abea9ae7db6c829fdddc20
t1nm1ksun <minsuk07672@naver.com>
2024-10-07 23:18:32 +0900

    [feat] #1 MyPage 구현

--- app/src/main/java/org/sopt/and/MyActivity.kt
+++ app/src/main/java/org/sopt/and/MyActivity.kt
@@ -3,17 +3,32 @@ package org.sopt.and
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.component.MyPageBox
+import org.sopt.and.component.MyPagePurchaseBox
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
 class MyActivity : ComponentActivity() {
@@ -40,11 +55,49 @@ fun MyPageScreen(modifier: Modifier, email: String) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(color = Color.White)
-            .padding(horizontal = 30.dp),
+            .background(color = Color(0xFF1B1B1B)),
     ) {
-        Text(text = email)
-
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color(0xFF252525))
+                .padding(top = 20.dp, start = 20.dp, end = 20.dp, bottom = 5.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painterResource(id = R.drawable.ic_my_page_person_24),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(end = 20.dp)
+                    .size(60.dp)
+            )
+            Text(
+                text = email, color = Color.White,
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                painterResource(id = R.drawable.ic_my_page_bell_24),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(end = 20.dp)
+                    .size(30.dp)
+            )
+            Icon(
+                painterResource(id = R.drawable.ic_my_page_settings_24),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .size(30.dp)
+            )
+        }
+        MyPagePurchaseBox(description = stringResource(R.string.my_page_first_box_description))
+        Spacer(modifier = Modifier.height(1.dp))
+        MyPagePurchaseBox(description = stringResource(R.string.my_page_second_box_description))
+        MyPageBox(title = stringResource(R.string.my_page_view_history), description = stringResource(R.string.my_page_view_history_description))
+        MyPageBox(title = stringResource(R.string.my_page_favorite_program), description = stringResource(R.string.my_page_favorite_program_description))
     }
 }
 
@@ -53,7 +106,7 @@ fun MyPageScreen(modifier: Modifier, email: String) {
 fun MyPageScreenPreview() {
     MyPageScreen(
         modifier = Modifier,
-        email = ""
+        email = "닉네임"
     )
 }
 
---
+++ app/src/main/java/org/sopt/and/component/MyPageBox.kt
@@ -0,0 +1,99 @@
+package org.sopt.and.component
+
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.R
+
+@Composable
+fun MyPagePurchaseBox(description: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color(0xFF252525))
+            .padding(top = 10.dp, start = 20.dp, end = 20.dp, bottom = 25.dp)
+    ) {
+
+        Text(text = description, color = Color(0xFFA5A5A5),
+            modifier = Modifier.padding(bottom = 5.dp))
+        Row (verticalAlignment = Alignment.CenterVertically){
+            Text(text = stringResource(R.string.my_page_purchase), color = Color.White)
+            Column {
+                Spacer(modifier = Modifier.height(2.dp))
+                Icon(
+                    painterResource(id = R.drawable.ic_my_page_arrow_forward_24),
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier
+                        .padding(start = 5.dp)
+                        .size(15.dp),
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun MyPageBox(title:String,description: String) {
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 11.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(
+            text = title,
+            color = Color.White,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 15.dp)
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        Image(
+            painter = painterResource(id = R.drawable.img_my_page_alert),
+            contentDescription = null,
+            modifier = Modifier.size(60.dp)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = description,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            color = Color(0xFFACACAC)
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+    }
+    
+}
+
+@Preview
+@Composable
+fun MyPageBoxPreView() {
+    Column {
+        MyPagePurchaseBox(description = "첫 결제 시 첫 달 100원!")
+        MyPageBox(title = stringResource(R.string.my_page_view_history), description = stringResource(R.string.my_page_view_history_description))
+    }
+}
